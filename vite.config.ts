45bdd100301a04c8e71cb03c90a6a652afe8f4a6
FranciscoPereira9 <43917604+FranciscoPereira9@users.noreply.github.com>
2026-03-25 17:02:30 +0000

    base change

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
   //   host: "::",
   //   port: 8080,
   // },
-  base: "/maria-e-francisco/",
+  base: "/",
   plugins: [react(), mode === "development" && componentTagger()].filter(
     Boolean,
   ),
